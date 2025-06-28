0977e9c06c6c8da7ed4cc35a69947e22419c618e
Arian Pieroni <rizelloyd@gmail.com>
2025-06-28 14:53:59 +0200

    properly implemented read from file
    Signed-off-by: Arian Pieroni <rizelloyd@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
-use std::env;
+use std::env::args;
+use std::fs::read_to_string;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = args().collect();
     let mut hashed_password: String = "".to_owned();
     let mut salt: String = "".to_owned();
     let mut password_list_path: String = "".to_owned();
-    
-    if args.len() == 2 {
-        hashed_password = args[0].clone();
-        password_list_path = args[1].clone();
-    } else if args.len() == 3 {
-        hashed_password = args[0].clone();
-        salt = args[1].clone();
+
+    if args.len() == 3 {
+        hashed_password = args[1].clone();
         password_list_path = args[2].clone();
+    } else if args.len() == 4 {
+        hashed_password = args[1].clone();
+        salt = args[2].clone();
+        password_list_path = args[3].clone();
     } else {
         print_usage();
         return;
     }
     drop(args);
+    let password_list = get_password_list(password_list_path.as_str());
+    drop(password_list_path);
+}
+
+fn get_password_list(path: &str) -> Vec<String> {
+    let mut contents: Vec<String> = Vec::new();
+    for line in read_to_string(path).unwrap().lines() {
+        contents.push(line.to_string());
+    }
+    contents
 }
 
 fn print_usage() {
