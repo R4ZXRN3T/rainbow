4320ffad03f43da177068beefe88fa8caec9ab77
Arian Pieroni <rizelloyd@gmail.com>
2025-09-11 17:59:52 +0200

    changed argument handler to clap, added salting to hashing operation, added verbosity level

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,124 @@
+use clap::{Parser, Subcommand};
 use sha2::{Digest as Sha2Digest, Sha224, Sha256, Sha384, Sha512, Sha512_224, Sha512_256};
-use std::env::args;
 use std::fs::read_to_string;
 use std::io::stdout;
 use std::io::Error;
 use std::io::Write;
 use std::time::SystemTime;
 
+#[derive(Parser, Debug)]
+#[command(
+	name = "rainbow",
+	version,
+	about = "A tool for hashing and cracking hashes.",
+	long_about = "\nUsage: rainbow [<Operation>] [<Arguments>]\n\n\
+Operation crack:\n\
+Usage: rainbow crack --input <Hash> [--salt <Salt>] [--multiplier <Multiplier>] --password-list <Password list>\n\n\
+\tHash:           The hashed password you want to crack\n\
+\tSalt:           The salt used for generating the password. This is optional\n\
+\tMultiplier:     The number of times the password was hashed. This is optional\n\
+\tPassword list:  The path to the password list you want to go through\n\n\
+Operation hash:\n\
+Usage: rainbow hash --algorithm <Algorithm> [--multiplier <Multiplier>] [--salt <Salt>] --input <String>\n\n\
+\tAlgorithm:      The Algorithm you want to use for hashing. Supported: sha224, sha256, sha384, sha512, sha512_224, sha512_256, md5\n\
+\tMultiplier:     The amount of times the password should be hashed. Optional parameter.\n\
+\tSalt:           A salt to add of the end of the string. Only relevant with multiplier enabled\n\
+\tString:         The string you want to hash"
+)]
+struct Args {
+	#[command(subcommand)]
+	command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+	#[command(about = "Hash a string using a specified algorithm")]
+	Hash(HashArgs),
+	#[command(about = "Crack a hash using a password list")]
+	Crack(CrackArgs),
+}
+
+#[derive(Parser, Debug)]
+struct HashArgs {
+	#[arg(
+		short = 'a',
+		long = "algorithm",
+		help = "Hash algorithm (sha224, sha256, sha384, sha512, sha512_224, sha512_256, md5)"
+	)]
+	algorithm: String,
+	#[arg(
+		short = 'm',
+		long = "multiplier",
+		default_value_t = 1,
+		help = "Number of times to hash"
+	)]
+	multiplier: i32,
+	#[arg(short = 'i', long = "input", help = "String to hash")]
+	string: String,
+	#[arg(short = 's', long = "salt", default_value = "", help = "Optional salt")]
+	salt: String,
+	#[arg(
+		long = "verbosity",
+		default_value_t = 2,
+		help = "Verbosity level (0-2)"
+	)]
+	verbosity_level: i32,
+}
+
+#[derive(Parser, Debug)]
+struct CrackArgs {
+	#[arg(short = 'i', long = "input", help = "Hash to crack")]
+	hash: String,
+	#[arg(short = 's', long = "salt", default_value = "", help = "Optional salt")]
+	salt: String,
+	#[arg(
+		short = 'l',
+		long = "password-list",
+		help = "Path to password list file"
+	)]
+	password_list: String,
+	#[arg(
+		short = 'm',
+		long = "multiplier",
+		default_value_t = 1,
+		help = "Number of times to hash (default: 1)"
+	)]
+	multiplier: i32,
+	#[arg(
+		long = "verbosity",
+		default_value_t = 2,
+		help = "Verbosity level (0-2)"
+	)]
+	verbosity_level: i32,
+}
 fn main() -> Result<(), &'static str> {
-	let args: Vec<String> = args().collect();
+	let args = Args::parse();
 
-	if args.len() < 2 {
-		print_usage();
-		return Err("Invalid operation");
-	}
-
-	if args[1] == "crack" {
-		crack_hash(&args)
-	} else if args[1] == "hash" {
-		hash(&args)
-	} else {
-		print_usage();
-		Err("Invalid operation")
+	match args.command {
+		Commands::Hash(hash_args) => hash(
+			hash_args.algorithm,
+			hash_args.multiplier,
+			hash_args.string,
+			hash_args.salt,
+			hash_args.verbosity_level,
+		),
+		Commands::Crack(crack_args) => crack_hash(
+			crack_args.hash,
+			crack_args.salt,
+			crack_args.multiplier,
+			crack_args.password_list,
+			crack_args.verbosity_level,
+		),
 	}
 }
 
-fn hash(args: &Vec<String>) -> Result<(), &'static str> {
-	let (algorithm_string, multiplier, to_hash) = match args.len() {
-		4 => (args[2].clone(), 1, args[3].clone()),
-		5 => (
-			args[2].clone(),
-			args[3].parse::<i32>().unwrap(),
-			args[4].clone(),
-		),
-		_ => {
-			print_usage();
-			return Err("Error: Invalid number of arguments");
-		}
-	};
-
+fn hash(
+	algorithm_string: String,
+	multiplier: i32,
+	to_hash: String,
+	salt: String,
+	verbosity_level: i32,
+) -> Result<(), &'static str> {
 	let algorithm = match algorithm_string.to_lowercase().as_str() {
 		"sha224" => HashType::Sha224,
 		"sha-224" => HashType::Sha224,
@@ -56,49 +137,51 @@ fn hash(args: &Vec<String>) -> Result<(), &'static str> {
 	};
 
 	let start_time = SystemTime::now();
-	let hashed_string = algorithm.hash(&to_hash, multiplier);
+	let hashed_string = algorithm.hash(&to_hash, multiplier, &salt);
 	let total_milliseconds: f64 =
 		(start_time.elapsed().expect("Error getting time").as_nanos() as f64) / 1000000.0;
 
-	println!(
-		"\nHashed string: {}\n took: {}ms\n",
-		hashed_string, total_milliseconds
-	);
+	match verbosity_level {
+		0 => println!("{}", hashed_string),
+		1 => println!("\nHashed string: {}\n", hashed_string),
+		_ => println!(
+			"\nHashed string: {}\n took: {}ms\n",
+			hashed_string, total_milliseconds
+		),
+	}
 
 	Ok(())
 }
 
-fn crack_hash(args: &Vec<String>) -> Result<(), &'static str> {
-	let (original_hashed_password, salt, password_list_path, multiplier) = match args.len() {
-		4 => (args[2].clone(), String::new(), args[3].clone(), 1),
-		5 => {
-			if let Ok(m) = args[3].parse::<i32>() {
-				(args[2].clone(), String::new(), args[4].clone(), m)
-			} else {
-				(args[2].clone(), args[3].clone(), args[4].clone(), 1)
-			}
-		}
-		6 => (
-			args[2].clone(),
-			args[3].clone(),
-			args[5].clone(),
-			args[4].parse::<i32>().unwrap_or(1),
-		),
-		_ => {
-			print_usage();
-			return Err("Error: Invalid number of arguments.");
-		}
-	};
+fn crack_hash(
+	original_hashed_password: String,
+	salt: String,
+	multiplier: i32,
+	password_list_path: String,
+	verbosity_level: i32,
+) -> Result<(), &'static str> {
+	if verbosity_level != 0 {
+		print!("\nReading list...");
+		stdout().flush().expect("Error flushing console");
+	}
+	let total_start_time = SystemTime::now();
 
-	print!("\nReading list...");
-	stdout().flush().expect("Error flushing console");
 	let mut start_time = SystemTime::now();
 	let password_list = get_password_list(&password_list_path)
 		.map_err(|_| "Error: Failed to read password list file")?;
 	let total_milliseconds: f64 =
 		(start_time.elapsed().expect("Error getting time").as_nanos() as f64) / 1000000.0;
-	print!(" Done! in {}ms\n", total_milliseconds);
-	stdout().flush().expect("Error flushing console");
+	match verbosity_level {
+		0 => (),
+		1 => {
+			print!(" Done!\n");
+			stdout().flush().expect("Error flushing console");
+		}
+		_ => {
+			print!(" Done! in {}ms\n", total_milliseconds);
+			stdout().flush().expect("Error flushing console");
+		}
+	}
 
 	let hash_types = HashType::from_length(original_hashed_password.len());
 	if hash_types.is_empty() {
@@ -109,20 +192,31 @@ fn crack_hash(args: &Vec<String>) -> Result<(), &'static str> {
 		return Err("Unsupported hash length");
 	}
 
-	print!("Hashing list...");
-	stdout().flush().expect("Error flushing console");
+	if verbosity_level != 0 {
+		print!("Hashing list...");
+		stdout().flush().expect("Error flushing console");
+	}
 
 	start_time = SystemTime::now();
 	for hash_type in hash_types {
 		if let Some(password) =
 			hash_type.check(&original_hashed_password, &salt, &password_list, multiplier)
 		{
-			let total_milliseconds: f64 =
+			let hash_milliseconds: f64 =
 				(start_time.elapsed().expect("Error getting time").as_nanos() as f64) / 1000000.0;
-			print!(
-				" Done!\n\nSuccess! Password found: {}\ntook: {}ms",
-				password, total_milliseconds
-			);
+			let total_milliseconds: f64 = (total_start_time
+				.elapsed()
+				.expect("Error getting time")
+				.as_nanos() as f64)
+				/ 1000000.0;
+			match verbosity_level {
+				0 => println!("{}", password),
+				1 => println!(" Done!\n\nHashed string: {}\n", password),
+				_ => println!(
+					" Done! in {}ms\n\nHashed string: {}\n took: {}ms\n",
+					hash_milliseconds, password, total_milliseconds
+				),
+			}
 			return Ok(());
 		}
 	}
@@ -161,15 +255,15 @@ impl HashType {
 		}
 	}
 
-	fn hash(&self, to_hash: &str, multiplier: i32) -> String {
+	fn hash(&self, to_hash: &str, multiplier: i32, salt: &str) -> String {
 		match self {
-			Self::Md5 => hash_md5(to_hash, multiplier),
-			Self::Sha224 => hash_sha2::<Sha224>(to_hash, multiplier),
-			Self::Sha256 => hash_sha2::<Sha256>(to_hash, multiplier),
-			Self::Sha384 => hash_sha2::<Sha384>(to_hash, multiplier),
-			Self::Sha512 => hash_sha2::<Sha512>(to_hash, multiplier),
-			Self::Sha512_224 => hash_sha2::<Sha512_224>(to_hash, multiplier),
-			Self::Sha512_256 => hash_sha2::<Sha512_256>(to_hash, multiplier),
+			Self::Md5 => hash_md5(to_hash, multiplier, salt),
+			Self::Sha224 => hash_sha2::<Sha224>(to_hash, multiplier, salt),
+			Self::Sha256 => hash_sha2::<Sha256>(to_hash, multiplier, salt),
+			Self::Sha384 => hash_sha2::<Sha384>(to_hash, multiplier, salt),
+			Self::Sha512 => hash_sha2::<Sha512>(to_hash, multiplier, salt),
+			Self::Sha512_224 => hash_sha2::<Sha512_224>(to_hash, multiplier, salt),
+			Self::Sha512_256 => hash_sha2::<Sha512_256>(to_hash, multiplier, salt),
 		}
 	}
 
@@ -255,38 +349,18 @@ fn check_md5(
 	})
 }
 
-fn hash_sha2<D: Sha2Digest>(to_hash: &str, multiplier: i32) -> String {
+fn hash_sha2<D: Sha2Digest>(to_hash: &str, multiplier: i32, salt: &str) -> String {
 	let mut final_string = to_hash.to_owned();
 	for _i in 0..multiplier {
-		final_string = hex::encode(D::digest(format!("{}", final_string)));
+		final_string = hex::encode(D::digest(format!("{}", final_string + salt)));
 	}
 	final_string
 }
 
-fn hash_md5(to_hash: &str, multiplier: i32) -> String {
+fn hash_md5(to_hash: &str, multiplier: i32, salt: &str) -> String {
 	let mut final_string = to_hash.to_owned();
 	for _i in 0..multiplier {
-		final_string = format!("{:x}", md5::compute(format!("{}", final_string)));
+		final_string = format!("{:x}", md5::compute(format!("{}", final_string + salt)));
 	}
 	final_string
-}
-
-fn print_usage() {
-	eprintln!("Wrong Arguments!\n");
-	eprintln!("Usage: rainbow [<Operation>] [<Arguments>]");
-	eprintln!("\nOperation crack:\n");
-	eprintln!("Usage: rainbow crack [<Hash>] [<Salt>] [<Password list>]\n");
-	eprintln!("\tHash:\t\tThe hashed password you want to crack");
-	eprintln!("\tSalt:\t\tThe salt used for generating the password. This is optional");
-	eprintln!("\tPassword list:\tThe path to the password list you want to go through");
-	eprintln!("\nOperation hash:\n");
-	eprintln!("Usage: rainbow hash [<Algorithm>] [<Multiplier>] [<String>]\n");
-	eprintln!(
-		"\tAlgorithm:\tThe Algorithm you want to use for hashing. Supported: sha224, sha256, sha384, sha512, sha512_224, sha512_256, md5"
-	);
-	eprintln!(
-		"\tMultiplier:\tThe amount of times the password should be hashed. Optional parameter."
-	);
-	eprintln!("\tString:\t\tThe string you want to hash");
-	eprintln!();
 }
